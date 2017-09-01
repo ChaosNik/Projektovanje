07caecfac24e6b8d9124a2330721584a9ca1c296
Nikola Karpić <nikola_karpic@live.com>
2017-09-01 17:32:37 +0200

    Create Poslovi.cpp

---
+++ Poslovi.cpp
@@ -0,0 +1,36 @@
+//**Stavke za projektni zadatak**//
+
+/*Recepcija*/
+  1.
+  2.
+/***********/
+  
+/*Zaduzenje*/
+  1.
+  2.
+/***********/
+  
+/*Razduzenje*/
+  1.
+  2.
+/************/
+  
+/*Rezervisanje*/
+  1.
+  2.
+/**************/
+  
+/*Izrada dokumenata*/
+  1.
+  2.
+/*******************/
+  
+/*Update*/
+  1.
+  2.
+/********/
+  
+/*Nazad*/
+  1.
+  2.
+/*******/
